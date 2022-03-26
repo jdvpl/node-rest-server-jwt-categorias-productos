b500945e8dd1b5ec3d9ea0f78ba6354a8c3411fa
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-03-25 20:58:56 -0500

    modelo de categoria

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Categoria= require('./categoria');
+const Role=require('./rol');
+const User= require('./user');
+const Server= require('./server');
+
+module.exports ={
+  Categoria,
+Role,
+User,
+Server,
+}
