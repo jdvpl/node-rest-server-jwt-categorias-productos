5c62f635415765d64a14e35725bf3f6781714367
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-03-26 18:45:23 -0500

    busqueda en la collection de usuarios

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,9 +1,51 @@
 const { response } = require("express");
+const { User } = require("../models");
+const {ObjectId}=require("mongoose").Types
+const collectionsAvailable=[
+  'categorias',
+  'productos',
+  'roles',
+  'users',
+]
 
-const buscar=(req,res=response)=>{
+
+const buscarUsuarios = async(termino="", res=response) => {
+  const esMongoId=ObjectId.isValid(termino)// retorna verdadero si es un mondo id
+
+  if(esMongoId){
+    const user = await User.findById(termino)
+
+    return res.status(200).json({
+      results: (user)?[user] :[]
+    })
+  }
+}
+
+
+const buscar=async(req,res=response)=>{
   const {collection,termino}=req.params;
 
-  return res.status(200).json({collection,termino})
+  if(!collectionsAvailable.includes(collection)){
+    return res.status(401).json({ msg: `La conecciones permitidas son ${collectionsAvailable}`})
+  }
+
+  switch (collection) {
+    case 'categorias':
+
+      break;
+
+    case 'productos':
+
+      break;
+
+    case 'users':
+      buscarUsuarios(termino,res)
+      break;
+    default:
+        res.status(404).json({msg: `Esta busqueda no esta permitida`})
+      break;
+  }
+
 }
 
 module.exports ={buscar}
